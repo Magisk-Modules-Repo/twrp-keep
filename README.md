b5be1a11deb8879f4be0cdbc6d4ce1aceae3e840
osm0sis <osm0sis@outlook.com>
2018-09-24 16:50:15 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# TWRP A/B Retention Script
+### osm0sis @ xda-developers
+*Keep TWRP installed after an A/B OTA*
+
+### Links
+* [GitHub](https://github.com/Magisk-Modules-Repo/TWRP-Keep)
+* [Support](https://forum.xda-developers.com/showthread.php?t=2239421)
+* [Donate](https://forum.xda-developers.com/donatetome.php?u=4544860)
+
+### Description
+**This is NOT a normal module - it will NOT show up in Magisk Manager's installed modules list.**
+
+Flash this script zip after the OTA has installed, and before you install Magisk to Inactive Slot from Manager
---
+++ config.sh
@@ -0,0 +1,48 @@
+##########################################################################################
+#
+# Magisk Module Template Config Script
+# by topjohnwu
+#
+##########################################################################################
+
+##########################################################################################
+# Installation Message
+##########################################################################################
+
+# Set what you want to show when installing your mod
+
+print_modname() {
+  ui_print "*******************************"
+  ui_print "   TWRP A/B Retention Script   "
+  ui_print "  by osm0sis @ xda-developers  "
+  ui_print "*******************************"
+}
+
+##########################################################################################
+# Custom Functions
+##########################################################################################
+
+# This file (config.sh) will be sourced by the main flash script after util_functions.sh
+# If you need custom logic, please add them here as functions, and call these functions in
+# update-binary. Refrain from adding code directly into update-binary, as it will make it
+# difficult for you to migrate your modules to newer template versions.
+# Make update-binary as clean as possible, try to only do function calls in it.
+
+[ $BOOTMODE ] || abort "*** Flashable manually from Magisk Manager only! ***"
+
+find_alt_boot_image() {
+  BOOTIMAGE=
+  case $SLOT in
+    _a) ALTSLOT=_b;;
+    _b) ALTSLOT=_a;;
+  esac
+  if [ ! -z $ALTSLOT ]; then
+    BOOTIMAGE=`find_block boot$ALTSLOT ramdisk$ALTSLOT`
+  else
+    BOOTIMAGE=`find_block boot ramdisk boot_a kern-a android_boot kernel lnx bootimg`
+  fi
+  if [ -z $BOOTIMAGE ]; then
+    # Lets see what fstabs tells me
+    BOOTIMAGE=`grep -v '#' /etc/*fstab* | grep -E '/boot[^a-zA-Z]' | grep -oE '/dev/[a-zA-Z0-9_./-]*' | head -n 1`
+  fi
+}
