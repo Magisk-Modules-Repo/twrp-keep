5ebbd702df572664ccac20143fa6bcecb679ad16
osm0sis <osm0sis@outlook.com>
2020-11-29 20:58:02 -0400

    fix find_manager_apk fallback on newer magisk.db
    - the strings fallback was broken when the preceding character changed from 5 to ! recently, this new regex should cover any preceding character going forward
    - will need to wait for https://github.com/topjohnwu/Magisk/pull/3527 to be merged to actually be resolved however, since the fallback here would never be reached on modern Magisk

--- customize.sh
+++ customize.sh
@@ -3,16 +3,17 @@ SKIPUNZIP=1
 type flash_image >/dev/null 2>&1 || flash_image() { flash_boot_image "$@"; }
 
 type find_manager_apk >/dev/null 2>&1 || find_manager_apk() {
+  local DBAPK
   [ -z $APK ] && APK=/data/adb/magisk.apk
   [ -f $APK ] || APK=/data/magisk/magisk.apk
   [ -f $APK ] || APK=/data/app/com.topjohnwu.magisk*/*.apk
   if [ ! -f $APK ]; then
-    DBAPK=`magisk --sqlite "SELECT value FROM strings WHERE key='requester'" 2>/dev/null | cut -d= -f2`
-    [ -z $DBAPK ] && DBAPK=`strings /data/adb/magisk.db | grep 5requester | cut -c11-`
+    DBAPK=$(magisk --sqlite "SELECT value FROM strings WHERE key='requester'" 2>/dev/null | cut -d= -f2)
+    [ -z $DBAPK ] && DBAPK=$(strings /data/adb/magisk.db | grep -E '^.requester.' | cut -c11-)
     [ -z $DBAPK ] || APK=/data/user_de/*/$DBAPK/dyn/*.apk
     [ -f $APK ] || [ -z $DBAPK ] || APK=/data/app/$DBAPK*/*.apk
   fi
-  [ -f $APK ] || ui_print "! Unable to detect Magisk Manager APK"
+  [ -f $APK ] || ui_print "! Unable to detect Magisk Manager APK for BootSigner"
 }
 
 bootsign_test() {
